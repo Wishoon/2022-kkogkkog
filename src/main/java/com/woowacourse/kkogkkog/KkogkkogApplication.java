15840241b1eb7bbb572690eb88eeca7863e23ee8
Wishoon <heonga2@gmail.com>
2022-11-21 23:34:59 +0900

    chore: 프로젝트 코드 설정

---
+++ src/main/java/com/woowacourse/kkogkkog/KkogkkogApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.kkogkkog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkogkkogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkogkkogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowacourse/kkogkkog/KkogkkogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.kkogkkog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkogkkogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
