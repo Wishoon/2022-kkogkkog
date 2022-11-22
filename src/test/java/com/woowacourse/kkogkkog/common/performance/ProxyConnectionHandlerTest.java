4d103b10a99d02ec164feea080fbf9d12a62cf7e
Wishoon <heonga2@gmail.com>
2022-11-22 22:23:11 +0900

    fix: 전체 테스트시 DataSource의 username과 password를 가져오지 못하는 문제 수정

--- src/test/java/com/woowacourse/kkogkkog/common/performance/ProxyConnectionHandlerTest.java
+++ src/test/java/com/woowacourse/kkogkkog/common/performance/ProxyConnectionHandlerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import java.sql.DriverManager;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.cglib.proxy.Proxy;
@@ -19,7 +20,7 @@ class ProxyConnectionHandlerTest {
 
     @Test
     void prepareStatement_메서드를_실행하는_경우_프록시_객체를_반환한다() throws Throwable {
-        try (final var connection = DriverManager.getConnection("jdbc:h2:mem:kkogkkog")) {
+        try (final var connection = DriverManager.getConnection("jdbc:h2:mem:kkogkkog", "sa", "")) {
             var proxyConnectionHandler = new ProxyConnectionHandler(connection, performanceMonitor);
             var prepareStatementMethod = connection.getClass().getMethod("prepareStatement", String.class);
 
@@ -32,7 +33,7 @@ class ProxyConnectionHandlerTest {
 
     @Test
     void prepareStatement_메서드를_실행하지_않는_경우_실제_객체를_반환한다() throws Throwable {
-        try (final var connection = DriverManager.getConnection("jdbc:h2:mem:kkogkkog")) {
+        try (final var connection = DriverManager.getConnection("jdbc:h2:mem:kkogkkog", "sa", "")) {
             var proxyConnectionHandler = new ProxyConnectionHandler(connection, performanceMonitor);
             var prepareStatementMethod = connection.getClass().getMethod("isReadOnly");
 
