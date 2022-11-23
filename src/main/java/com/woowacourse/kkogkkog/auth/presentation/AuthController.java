61850cd5068246fa4669763f3981a83f658b49ca
Wishoon <heonga2@gmail.com>
2022-11-24 00:31:54 +0900

    feat: OAuth 기반의 토큰을 발급하는 API 구현

---
+++ src/main/java/com/woowacourse/kkogkkog/auth/presentation/AuthController.java
@@ -0,0 +1,32 @@
+package com.woowacourse.kkogkkog.auth.presentation;
+
+import com.woowacourse.kkogkkog.auth.application.AuthService;
+import com.woowacourse.kkogkkog.auth.application.OAuthService;
+import com.woowacourse.kkogkkog.auth.application.dto.request.AccessTokenRequest;
+import com.woowacourse.kkogkkog.auth.application.dto.response.AccessTokenResponse;
+import com.woowacourse.kkogkkog.auth.infrastructure.client.dto.response.OAuthProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final OAuthService oAuthService;
+    private final AuthService authService;
+
+    @PostMapping("/{oauthProvider}/token")
+    public ResponseEntity<AccessTokenResponse> createAccessToken(@PathVariable final String oauthProvider,
+                                                                 @RequestBody final AccessTokenRequest request) {
+        OAuthProfileResponse oauthProfile = oAuthService.getOauthProfile(oauthProvider, request);
+        AccessTokenResponse tokenResponse = authService.createAccessToken(oauthProfile);
+
+        return ResponseEntity.ok(tokenResponse);
+    }
+}
