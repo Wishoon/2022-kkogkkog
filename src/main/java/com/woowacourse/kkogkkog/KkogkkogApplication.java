13833b14bdc0a17c0a6e18174bf7a074b3a27ff9
Wishoon <heonga2@gmail.com>
2022-11-24 15:46:56 +0900

    feat: @EnableJpaAuditing을 통한 BaseEntity 기능 구현

--- src/main/java/com/woowacourse/kkogkkog/KkogkkogApplication.java
+++ src/main/java/com/woowacourse/kkogkkog/KkogkkogApplication.java
@@ -2,12 +2,14 @@ package com.woowacourse.kkogkkog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KkogkkogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KkogkkogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KkogkkogApplication.class, args);
+    }
 
 }
