e22d8c6859a02ff47395b9950b4c2b8905715981
Wishoon <heonga2@gmail.com>
2022-11-26 20:35:55 +0900

    feat: 쿠폰의 상태를 변경하는 API 구현

--- src/main/java/com/woowacourse/kkogkkog/coupon/presentation/CouponController.java
+++ src/main/java/com/woowacourse/kkogkkog/coupon/presentation/CouponController.java
@@ -2,6 +2,7 @@ package com.woowacourse.kkogkkog.coupon.presentation;
 
 import com.woowacourse.kkogkkog.coupon.application.CouponQueryService;
 import com.woowacourse.kkogkkog.coupon.application.CouponService;
+import com.woowacourse.kkogkkog.coupon.application.dto.request.CouponConditionUpdateRequest;
 import com.woowacourse.kkogkkog.coupon.application.dto.request.CouponCreateRequest;
 import com.woowacourse.kkogkkog.coupon.application.dto.response.CouponResponse;
 import java.net.URI;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,16 +25,25 @@ public class CouponController {
     private final CouponQueryService couponQueryService;
 
     @PostMapping
-    public ResponseEntity<Void> createCoupon(@RequestBody final CouponCreateRequest request) {
+    public ResponseEntity<Void> create(@RequestBody final CouponCreateRequest request) {
         Long couponId = couponService.create(request);
 
         return ResponseEntity.created(URI.create("/coupon/" + couponId)).build();
     }
 
     @GetMapping("/{couponId}")
-    public ResponseEntity<CouponResponse> selectCoupon(@PathVariable Long couponId) {
+    public ResponseEntity<CouponResponse> select(@PathVariable Long couponId) {
         CouponResponse response = couponQueryService.find(couponId);
 
         return ResponseEntity.ok(response);
+    }
+
+    // @AuthMember 커스텀 어노테이션을 만들기 전까지 1L로 하드코딩
+    @PutMapping("/{couponId}/condition")
+    public ResponseEntity<Void> updateCondition(@PathVariable Long couponId,
+                                                @RequestBody CouponConditionUpdateRequest request) {
+        couponService.updateCondition(couponId, 1L, request);
+
+        return ResponseEntity.noContent().build();
     }
 }
