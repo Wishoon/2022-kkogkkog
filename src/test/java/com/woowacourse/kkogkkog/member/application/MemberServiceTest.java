0cd9edbb23bd14ec3ea089694f2a7de510a87853
Wishoon <heonga2@gmail.com>
2022-11-28 22:58:00 +0900

    refactor: 픽스쳐를 활용하여 회원 도메인 테스트 변경

--- src/test/java/com/woowacourse/kkogkkog/member/application/MemberServiceTest.java
+++ src/test/java/com/woowacourse/kkogkkog/member/application/MemberServiceTest.java
@@ -1,12 +1,13 @@
 package com.woowacourse.kkogkkog.member.application;
 
+import static com.woowacourse.kkogkkog.support.fixture.MemberFixture.인증된_회원;
+import static com.woowacourse.kkogkkog.support.fixture.MemberFixture.회원;
+import static com.woowacourse.kkogkkog.support.fixture.MemberFixture.회원을_생성하거나_변경하는_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woowacourse.kkogkkog.annotation.IntegrationTest;
 import com.woowacourse.kkogkkog.member.application.dto.MemberCreateOrUpdateRequest;
-import com.woowacourse.kkogkkog.member.domain.Member;
 import com.woowacourse.kkogkkog.member.domain.MemberRepository;
-import com.woowacourse.kkogkkog.member.domain.ProviderType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,11 +21,9 @@ class MemberServiceTest {
 
     @Test
     void 회원의_정보가_존재하지_않는_경우_회원을_생성한다() {
-        memberRepository.save(new Member(
-            null, "oauthId-1", "email@gmail.com", "username", "https://profile.com", ProviderType.GITHUB, false));
-        MemberCreateOrUpdateRequest request = new MemberCreateOrUpdateRequest(
-            "oauthId-1", "email@gmail.com", "username", "https://profile.com", "github");
+        memberRepository.save(회원());
 
+        MemberCreateOrUpdateRequest request = 회원을_생성하거나_변경하는_요청();
         memberService.createOrUpdate(request);
 
         assertThat(memberRepository.findAll()).hasSize(1);
@@ -32,8 +31,7 @@ class MemberServiceTest {
 
     @Test
     void 회원의_정보가_존재하는_경우_회원정보를_변경한다() {
-        MemberCreateOrUpdateRequest request = new MemberCreateOrUpdateRequest(
-            "oauthId-1", "email@gmail.com", "username", "https://profile.com", "github");
+        MemberCreateOrUpdateRequest request = 회원을_생성하거나_변경하는_요청();
 
         memberService.createOrUpdate(request);
 
@@ -42,9 +40,7 @@ class MemberServiceTest {
 
     @Test
     void 회원_ID를_통헤_인가여부를_조회할_수_있다() {
-        Long memberId = memberRepository.save(
-            new Member(null, "oauthId-1", "email@gmail.com", "username", "https://profile.com", ProviderType.GITHUB,
-                true)).getId();
+        Long memberId = memberRepository.save(인증된_회원()).getId();
 
         boolean actual = memberService.findApproval(memberId);
 
