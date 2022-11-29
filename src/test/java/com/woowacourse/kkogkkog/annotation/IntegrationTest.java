9cc451041730445d39945964218ceeb6095c138d
Wishoon <heonga2@gmail.com>
2022-11-29 14:11:41 +0900

    refactor: webEnvironment를 DEFINED_PORT로 변경하여 Context 통일

--- src/test/java/com/woowacourse/kkogkkog/annotation/IntegrationTest.java
+++ src/test/java/com/woowacourse/kkogkkog/annotation/IntegrationTest.java
@@ -1,19 +1,20 @@
 package com.woowacourse.kkogkkog.annotation;
 
 import com.woowacourse.kkogkkog.extension.DataCleanerExtension;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @ExtendWith(DataCleanerExtension.class)
 public @interface IntegrationTest {
+
 }
