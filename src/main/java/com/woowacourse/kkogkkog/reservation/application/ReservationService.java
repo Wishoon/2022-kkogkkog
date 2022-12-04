80dab18cb16e87c4ae1776162b3ea634722a5abf
Wishoon <heonga2@gmail.com>
2022-12-04 13:08:59 +0900

    refactor: 의존성 역전 원칙을 기반한 Validator를 통해 Coupon <-> Reservation 패키지 순환 참조 제거

--- src/main/java/com/woowacourse/kkogkkog/reservation/application/ReservationService.java
+++ src/main/java/com/woowacourse/kkogkkog/reservation/application/ReservationService.java
@@ -1,11 +1,11 @@
 package com.woowacourse.kkogkkog.reservation.application;
 
-import com.woowacourse.kkogkkog.coupon.domain.repository.CouponRepository;
 import com.woowacourse.kkogkkog.reservation.application.dto.ReservationCreateRequest;
 import com.woowacourse.kkogkkog.reservation.domain.Condition;
 import com.woowacourse.kkogkkog.reservation.domain.Reservation;
 import com.woowacourse.kkogkkog.reservation.domain.repository.ReservationRepository;
 import com.woowacourse.kkogkkog.reservation.domain.strategy.ConditionTypeRepository;
+import com.woowacourse.kkogkkog.reservation.domain.validator.ReservationValidator;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
@@ -17,23 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final ReservationValidator reservationValidator;
     private final ConditionTypeRepository conditionTypeRepository;
-    private final CouponRepository couponRepository;
     private final Clock clock;
 
     @Transactional
     public Long save(final Long memberId, final ReservationCreateRequest request) {
-        validateExistsCoupon(request.getCouponId());
+        reservationValidator.validateExistsCoupon(request.getCouponId());
         validateReservationAppointedTime(request.getAppointedTime());
         Reservation reservation = createReservation(memberId, request);
 
         return reservationRepository.save(reservation).getId();
-    }
-
-    private void validateExistsCoupon(final Long couponId) {
-        if (!couponRepository.existsById(couponId)) {
-            throw new IllegalArgumentException("존재하지 않는 쿠폰입니다.");
-        }
     }
 
     private void validateReservationAppointedTime(final LocalDateTime appointedTime) {
